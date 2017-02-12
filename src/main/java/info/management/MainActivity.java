4d38d722a713adfcb7922d3a626953ea3d770e10
glide112 <yosuke.sawamura@gmail.com>
2017-02-12 18:43:10 +0900

    Update MainActivity.java

--- src/main/java/info/management/MainActivity.java
+++ src/main/java/info/management/MainActivity.java
@@ -1,9 +1,23 @@
 package info.management;
 
-/**
- * Scopia Management API Client Sample
- * Created by Yosuke Sawamura on 2017/02/04.
- */
+/*
+MainActivity.java
+Scopia Management API Client Sample / written by Yosuke Sawamura on 2017/02/04.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
